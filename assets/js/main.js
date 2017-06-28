76cfbcd6a46f3e3d5c5ced9b4217328eb5017db4
Məhəmməd <mahammad.c@code.edu.az>
2017-06-28 18:53:58 +0500

    jafarli

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,5 +18,45 @@ $(document).ready(function(){
 				background:'transparent',
 			});
 		}
+		if (animate>660){
+			$(".html").css({
+				width:'90%',
+				transition:'.5s',
+			});
+			$(".css").css({
+				width:'90%',
+				transition:'.6s',
+			});
+			$(".js").css({
+				width:'70%',
+				transition:'.7s',
+			});
+			$(".php").css({
+				width:'75%',
+				transition:'.8s',
+			});
+			$(".ajax").css({
+				width:'50%',
+				transition:'.9s',
+			});
+			$(".pascal").css({
+				width:'50%',
+				transition:'1s',
+			});
+		}
+		if(animate>1450){
+			$(".hear_title").css({
+				transform: 'translateX(0px)',
+	      transition:'1s',
+			});
+			$(".hear_text").css({
+				transform: 'translateX(0px)',
+	      transition:'1s',
+			});
+			$(".hear_btn").css({
+				transform: 'translatey(0px)',
+	      transition:'1s',
+			});
+		}
 	});
 });
