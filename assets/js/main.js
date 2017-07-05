5fac9ac82828ff73f4a8be5f25eaf3c6525b2fa2
Məhəmməd <mahammad.c@code.edu.az>
2017-07-06 02:43:04 +0500

    jafarli

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,3 +65,13 @@ $(document).ready(function(){
 $(function(){
   $.scrollIt();
 });
+
+$(document).ready(function() {
+            var sideslider = $('[data-toggle=collapse-side]');
+            var sel = sideslider.attr('data-target');
+            var sel2 = sideslider.attr('data-target-2');
+            sideslider.click(function(event){
+                $(sel).toggleClass('in');
+                $(sel2).toggleClass('out');
+            });
+        });
